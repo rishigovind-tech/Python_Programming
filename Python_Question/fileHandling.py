5240b2ab0543706fe4e81c33d51f291454189407
RISHIGOVIND <rishigovind36@gmail.com>
2026-03-18 12:44:46 +0530

    Python Programming

---
+++ Python_Question/fileHandling.py
@@ -0,0 +1,39 @@
+# file = open(r'Data.txt', 'r')
+# data = file.read()
+# print(data)
+# file.close()
+
+# ==============
+
+# file=open('my.dat','w')
+# st= 'abcdefghijklmnopqr'
+# file.write(st)
+# file.close()
+
+# ========================
+
+# file=open('my.dat','r+')
+
+# print(file.read())
+
+# st= 'xyz'
+
+# file.write(st)
+
+# file.close()
+
+# ========================
+
+file=open('my.dat','w+')
+
+st= 'ABCDEFGH'
+file.write(st)
+
+file.seek(0,0)
+
+print(file.read())
+
+file.close()
+
+
+
