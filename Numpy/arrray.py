91fa6ea1243e80245aed16f500cd89a248898712
RISHIGOVIND <rishigovind36@gmail.com>
2026-03-24 10:35:38 +0530

    Python Programming

---
+++ Numpy/arrray.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+oneD=np.array([1,2,3,4,5,6,7,8,9,10])
+print(oneD) 
+print(oneD.ndim) 
+
+
+twoD=np.array([[1,2,3,4,5],[6,7,8,9,10]])
+print(twoD)
+print(twoD.ndim)
+
+threeD=np.array( [[[1,2],[3,4]],[[5,6],[7,8]] ])
+print(threeD)
+print(threeD.ndim)
+
+
